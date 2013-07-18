16d3ffa1c23d3152dc4e6a1954a7532e81f6060e
John Epperson <john.epperson@countrystone.com>
2013-07-17 20:44:48 -0500

    MOAR DEBUG

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require File.expand_path('../application', __FILE__)
 YAML.load_file("#{::Rails.root}/config/environment_variables.yml").each do |key, value|
   puts "Loading Variable [#{key}] = #{value}"
   ENV[key] = value
+  puts "Rails environment_variables: #{Env[key]}"
 end
 
 # Initialize the rails application
