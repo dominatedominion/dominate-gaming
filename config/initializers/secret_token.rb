e0856c5edabb6427b3e539c1228c23c60aba8331
John Epperson <j.epperson@verizon.net>
2013-07-18 00:13:41 +0000

    Loading All files in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DominateGaming::Application.config.secret_token = 'b0b024601ba109b822a0a071e32004208aaa66f22b1d23c05af35659596e59c989abc05656ae4c025186d10ac5c7bdfbcd0e63e32c1b907fef677e473449cfae'
